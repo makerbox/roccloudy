645deb50a5e2194ebc30b33c53b92b5dfff14e0d
makerbox <mattwerth@mattwerth.com>
2017-08-11 16:30:13 +1000

    local test urgh

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,28 +3,6 @@ class TestController < ApplicationController
 	
 	def index
        	@lastdiscount = Discount.all.last
-       	@results = []
-       	dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-
-
-      # -------------------------GET PRODUCTS AND CREATE / UPDATE PRODUCT RECORDS------------------------
-      	@products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-          @products.each do |p|
-            if p.Inactive == 0
-              # @code = p.Code.strip
-              # @qty = p.QtyInStock
-              # @rqty = p.QtyReserved
-              # @group = p.ProductGroup.to_s.strip
-              @pricecat = p.PriceCat.to_s.strip
-              # @results << @code
-              # @results << @qty
-              # @results << @rqty
-              # @results << @qty - @rqty
-              # @results << @group
-              @results << @pricecat
-          	end
-          end
-
-    	dbh.disconnect
+       	
   	end
 end
