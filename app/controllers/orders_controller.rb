9e162df0c2069ed8498a7aafcd76051e01ee9391
makerbox <mattwerth@mattwerth.com>
2017-08-11 10:00:14 +1000

    mobile view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -127,6 +127,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:total, :user_id, :notes, :cust_order_number, :order_number, :delivery_date)
+      params.require(:order).permit(:showcart, :total, :user_id, :notes, :cust_order_number, :order_number, :delivery_date)
     end
   end
