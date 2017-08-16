fb07860703f8f532edd8460df2a5632682951da1
makerbox <mattwerth@mattwerth.com>
2017-08-16 21:06:12 +1000

    qty test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,7 +2,21 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-       	@lastdiscount = Discount.all.last
-       	
+       	dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+       	@qty = []
+       	@qtyhand = []
+
+      # -------------------------GET PRODUCTS AND CREATE / UPDATE PRODUCT RECORDS------------------------
+      @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+          @products.each do |p|
+            if p.Inactive == 0
+              
+              @qty << p.QtyInStock
+              @qty << p.QtyReserve
+              @qty << qty - p.QtyReserve
+          end
+      end
+      dbh.disconnect
+
   	end
 end
