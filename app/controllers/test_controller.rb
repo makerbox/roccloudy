16310ad73533d3a1f2a93978f7ca4291b058e244
makerbox <mattwerth@mattwerth.com>
2017-08-17 12:27:13 +1000

    test on server

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,8 +2,28 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-       	
-       	@products = Product.all
+       	@results = []
+       	# -------------------------GET PRODUCTS AND CREATE / UPDATE PRODUCT RECORDS------------------------
+      @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+          @products.each do |p|
+            if p.Inactive == 0
+              code = p.Code.strip
+              if Product.all.where(code: code).exists?
+                filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
+                if File.exist?(filename)
+                  filedate = File.mtime(filename) #get date modified
+                  Product.all.find_by(code: code).update_attribute(imageurl: filedate) #imageurl isn't used for anything, so use it to keep track of updates
+                  # Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
+                  # stop from overloading transformations
+                  @result << filedate
+                else
+                  Product.all.find_by(code: code).destroy
+                end
+              end
+            end
+          end
+
+          dbh.disconnect
 
   	end
 end
--- app/jobs/populate_job.rb
+++ app/jobs/populate_job.rb
@@ -219,6 +219,8 @@ class PopulateJob
                 Product.all.find_by(code: code).update_attributes(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
                 filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
                 if File.exist?(filename)
+                  filedate = File.mtime(filename) #get date modified
+                  Product.all.find_by(code: code).update_attribute(imageurl: filedate) #imageurl isn't used for anything, so use it to keep track of updates
                   # Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
                   # stop from overloading transformations
                 else
