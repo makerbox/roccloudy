c5749081d23e10030c6ee182d915aa18d4cabdc8
makerbox <mattwerth@mattwerth.com>
2017-08-17 21:47:53 +1000

    all day sweep

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -11,26 +11,9 @@ class TestController < ApplicationController
           @products.each do |p|
             if p.Inactive == 0
               code = p.Code.strip
-              description = p.Description.to_s.strip
-              price1 = p.SalesPrice1
-              price2 = p.SalesPrice2
-              price3 = p.SalesPrice3
-              price4 = p.SalesPrice4
-              price5 = p.SalesPrice5
-              rrp = p.SalesPrice6
-              qty = p.QtyInStock
-              qty = qty - p.QtyReserve
-              if p.AllowDisc == 1
-                allow_disc = true
-              else
-                allow_disc = false
-              end
-              group = p.ProductGroup.to_s.strip
-              pricecat = p.PriceCat.to_s.strip
-              puts pricecat
               # # needs category
               if Product.all.where(code: code).exists?
-                Product.all.find_by(code: code).update_attributes(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
+                # Product.all.find_by(code: code).update_attributes(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
                 filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
                 if File.exist?(filename)
                   filedate = File.mtime(filename) #get date modified
@@ -40,18 +23,7 @@ class TestController < ApplicationController
                     Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
                   	@results << code
                   # end
-                else
-                  Product.all.find_by(code: code).destroy
                 end
-              else
-                newproduct = Product.new(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty, hidden: false)
-                filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
-                if File.exist?(filename)
-                  Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
-                  # stop from overloading transformations
-                  newproduct.save
-                end
-                
               end
             end
           end
