d011a6f7d91dad36e2475f2932356b1241f23ce3
makerbox <mattwerth@mattwerth.com>
2017-08-17 13:31:59 +1000

    argh

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -9,6 +9,8 @@ class TestController < ApplicationController
           @products.each do |p|
             if p.Inactive == 0
               code = p.Code.strip
+              qty = p.QtyInStock
+              qty = qty - p.QtyReserve
               if Product.all.where(code: code).exists?
                 filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
                 if File.exist?(filename)
@@ -16,9 +18,11 @@ class TestController < ApplicationController
                   # Product.all.find_by(code: code).update_attribute(imageurl: filedate) #imageurl isn't used for anything, so use it to keep track of updates
                   # Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
                   # stop from overloading transformations
-                  if Product.all.where(code: code).updated_at <= filedate
+                  if Product.all.find_by(code: code).updated_at <= filedate
                   	@results << filedate
                   end
+                  @results << code
+                  @results << qty
                 # else
                   # Product.all.find_by(code: code).destroy
                 end
