86b8b4ecfa1096fb7f63da5edab46f2cf12b7beb
makerbox <mattwerth@mattwerth.com>
2017-08-17 15:04:13 +1000

    just first fifty

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -6,17 +6,19 @@ class TestController < ApplicationController
        	dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
        	# -------------------------GET PRODUCTS AND CREATE / UPDATE PRODUCT RECORDS------------------------
       @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-          @products.each do |p|
+          @products.first(50).each do |p|
             if p.Inactive == 0
               code = p.Code.strip
               if Product.all.where(code: code).exists?
                 filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
                 if File.exist?(filename)
-                  filedate = File.mtime(filename) 
+                  filedate = File.mtime(filename).to_date
+                  productdate = Product.all.find_by(code: code).updated_at.to_date
+                  if filedate <= productdate
                   @results << 'file updated >'
                   @results << filedate
                   @results << 'product updated >'
-                  @results << Product.all.find_by(code: code).updated_at
+                  @results << productdate
                 end
               end
             end
