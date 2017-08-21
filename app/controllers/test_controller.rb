ce9b14b63bcfae745d0f0393ef2525f17984f2ec
makerbox <mattwerth@mattwerth.com>
2017-08-21 13:50:57 +1000

    testing

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,32 +3,37 @@ class TestController < ApplicationController
 	
 	def index
        	@results = []
-       	dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+
+
+        Account.all.each do |a|
+          @results << a.rep
+        end
+       	# dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
 
 
       # -------------------------GET PRODUCTS AND CREATE / UPDATE PRODUCT RECORDS------------------------
-      @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-          @products.first(100).each do |p|
-            if p.Inactive == 0
-              code = p.Code.strip
-              # # needs category
-              if Product.all.where(code: code).exists?
-                # Product.all.find_by(code: code).update_attributes(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
-                filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
-                if File.exist?(filename)
-                  filedate = File.mtime(filename) #get date modified
-                  lastupdated = Product.all.find_by(code: code).updated_at
-                  timediff = lastupdated - filedate
-                  # if timediff <= 400 #update image if it's under a day old
-                    Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
-                  	@results << code
-                  # end
-                end
-              end
-            end
-          end
+      # @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+      #     @products.first(100).each do |p|
+      #       if p.Inactive == 0
+      #         code = p.Code.strip
+      #         # # needs category
+      #         if Product.all.where(code: code).exists?
+      #           # Product.all.find_by(code: code).update_attributes(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
+      #           filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
+      #           if File.exist?(filename)
+      #             filedate = File.mtime(filename) #get date modified
+      #             lastupdated = Product.all.find_by(code: code).updated_at
+      #             timediff = lastupdated - filedate
+      #             # if timediff <= 400 #update image if it's under a day old
+      #               Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
+      #             	@results << code
+      #             # end
+      #           end
+      #         end
+      #       end
+      #     end
 
-          dbh.disconnect
+      #     dbh.disconnect
 
   	end
 end
