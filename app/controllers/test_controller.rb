4936106d6ce71565d6a4d1a6bab9546474db3d42
makerbox <mattwerth@mattwerth.com>
2017-11-15 11:16:48 +1100

    testing inactive

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,6 +3,20 @@ class TestController < ApplicationController
 	
 	def index
       
+dbh = RDBI.connect :ODBC, :db => "wholesaleportalnz"
+		@products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
 
+		@results = []
+		@products.each do |p|
+			if p.Inactive == 0
+				@results << p.Code
+				@results << '= inactive?'
+				@results << p.Inactive
+				@results << '............'
+			end
+		end
+
+
+		dbh.disconnect
   end
 end
