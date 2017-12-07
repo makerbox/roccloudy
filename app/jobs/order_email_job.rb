be9036c1d0b21d3dbd1c711616d9a129dc19d8f7
makerbox <mattwerth@mattwerth.com>
2017-12-07 15:29:45 +1100

    slight tweak to order email job

--- app/jobs/order_email_job.rb
+++ app/jobs/order_email_job.rb
@@ -3,9 +3,9 @@ class OrderEmailJob
 	include SuckerPunch::Job
 
   # The perform method is in charge of our code execution when enqueued.
-  def perform(order)
-  	  OrderMailer.order(order).deliver_now
-  	  OrderMailer.receipt(order).deliver_now
+  def perform(o)
+  	  OrderMailer.order(o).deliver_now
+  	  OrderMailer.receipt(o).deliver_now
   end
 
 end
