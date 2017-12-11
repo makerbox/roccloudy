53909634ccd13a9aeb5676f93a9837e34ba78685
makerbox <mattwerth@mattwerth.com>
2017-12-11 13:57:34 +1100

    testint mailer

--- app/jobs/order_email_job.rb
+++ app/jobs/order_email_job.rb
@@ -4,8 +4,8 @@ class OrderEmailJob
 
   # The perform method is in charge of our code execution when enqueued.
   def perform(o)
-  	  OrderMailer.order(o)
-  	  OrderMailer.receipt(o)
+  	  OrderMailer.order(o).deliver
+  	  OrderMailer.receipt(o).deliver
   end
 
 end
