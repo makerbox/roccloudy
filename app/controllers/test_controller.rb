f8707fccd15c9c5fa544d26587a6266de420c230
makerbox <mattwerth@mattwerth.com>
2017-12-13 13:24:39 +1100

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,64 +3,42 @@ class TestController < ApplicationController
 	
 	def index
       @results = []
-
-dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-		@products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-
-          @products.each do |p|
-            if p.Inactive == 0
-              code = p.Code.strip
-              description = p.Description.to_s.strip
-              price1 = p.SalesPrice1
-              price2 = p.SalesPrice2
-              price3 = p.SalesPrice3
-              price4 = p.SalesPrice4
-              price5 = p.SalesPrice5
-              rrp = p.SalesPrice6
-              qty = p.QtyInStock
-              qty = qty - p.QtyReserve
-              if p.AllowDisc == 1
-                allow_disc = true
-              else
-                allow_disc = false
-              end
-              group = p.ProductGroup.to_s.strip
-              pricecat = p.PriceCat.to_s.strip
-              # puts pricecat
-              @results << code
-              # # needs category
-              if Product.all.where(code: code).exists?
-                Product.all.find_by(code: code).update_attributes(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
-                filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
-                if File.exist?(filename)
-                  # Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
-                  # stop from overloading transformations
-                  @results << 'image exists'
-                else
-                	@results << 'no image'
-                  Product.all.find_by(code: code).destroy
-                end
-              else
-                newproduct = Product.new(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty, hidden: false)
-                filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
-                if File.exist?(filename)
-                	@results << 'new image'
-                  Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
-                  # stop from overloading transformations
-                  newproduct.save
-                end
-                
-              end
-            else
-              #destroy inactive
-              code = p.Code.strip
-              if thisprod = Product.all.find_by(code: code)
-                thisprod.destroy
-              end
+		counter = 0
+      dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+      @customers_ext = dbh.execute("SELECT * FROM customer_mastext").fetch(:all, :Struct)
+      @results << 'count from Attache = ' + @customers.count
+      @customers_ext.each do |ce|
+        counter += 1
+        code = ce.Code.strip
+        if ce.InactiveCust == 1
+          if Account.all.find_by(code: code)
+            account = Account.all.find_by(code: code) # if there is an attache inactive account already in the portal, we delete it and its user
+            @results << 'inactive in Attache' + account.code
+          end
+        else
+          email = ce.EmailAddr.strip
+          if !Account.all.find_by(code: code)
+            if email.blank?
+              email = counter
+            end
+            if !User.all.find_by(email: email)
+              @results << email
             end
           end
+        end
+      end
+      dbh.disconnect 
 
-          dbh.disconnect
+
+      dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+      @customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
+      @customers.each do |c|
+        code = c.Code.strip
+        if Account.all.find_by(code: code)
+        	@results << 'account exists ' + code
+        end
+      end
+      dbh.disconnect 
 
   end
 end
