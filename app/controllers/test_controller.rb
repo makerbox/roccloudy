4593899f559cf8bfeb6cd08b6de898f9f4f721b3
makerbox <mattwerth@mattwerth.com>
2017-12-13 15:46:18 +1100

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -4,12 +4,21 @@ class TestController < ApplicationController
 	def index
       @results = []
 
-      dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-      # @results = dbh.execute("INSERT INTO customer_master (Code, Name, Contact) VALUES ('test', 'test', 'test')")
-      # @results = dbh.execute("DELETE FROM customer_master WHERE Code = 'test'")
-      
+      Account.each do |a|
+      	if existing = Account.where('code LIKE ?', '%#{a.code}%')
+      		@results << 'found duplicates for ' + a.code
+      		existing.each do |e|
+      			@results << e.code
+      		end
+      	end
+      end
 
-      dbh.disconnect 
-      # redirect_to 'http://roccloudy.com'
+      # dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+      # # @results = dbh.execute("INSERT INTO customer_master (Code, Name, Contact) VALUES ('test', 'test', 'test')")
+      # # @results = dbh.execute("DELETE FROM customer_master WHERE Code = 'test'")
+
+
+      # dbh.disconnect 
+      # # redirect_to 'http://roccloudy.com'
   	end
 end
