684734b2da4707f858d62cbaabf7e2f11c1441e6
makerbox <mattwerth@mattwerth.com>
2017-12-13 15:29:05 +1100

    test

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
 	def after_sign_up_path_for(resource)
 		:new_account
 	end
+
+	def after_update_path_for(resource)
+      redirect_to 'http://218.214.78.21:3200?' + resource.email
+    end
 end
--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -5,10 +5,11 @@ class TestController < ApplicationController
       @results = []
 
       dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-      # @results = dbh.execute("DELETE INTO customer_master (Code, Name, Contact) VALUES ('test', 'test', 'test')")
-      @results = dbh.execute("DELETE FROM customer_master WHERE Code = 'test'")
+      # @results = dbh.execute("INSERT INTO customer_master (Code, Name, Contact) VALUES ('test', 'test', 'test')")
+      # @results = dbh.execute("DELETE FROM customer_master WHERE Code = 'test'")
+      @results = 
 
       dbh.disconnect 
-      redirect_to 'http://roccloudy.com'
+      # redirect_to 'http://roccloudy.com'
   	end
 end
