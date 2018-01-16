7262f7d1f038c6569d3d00ff948505908b2b4149
makerbox <mattwerth@mattwerth.com>
2018-01-17 10:18:23 +1100

    asdf

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,8 +2,12 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
+    @results = []
+          if params[:order]
             @results = params[:order]
-
+          else
+            @results = 'no params'
+          end
 
       # dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
       # # @results = dbh.execute("INSERT INTO customer_master (Code, Name, Contact) VALUES ('test', 'test', 'test')")
