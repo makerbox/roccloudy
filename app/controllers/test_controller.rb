816d83b763a4038056601990ee34fb392253d975
makerbox <mattwerth@mattwerth.com>
2018-01-29 18:37:41 +1100

    asdf

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -9,7 +9,7 @@ class TestController < ApplicationController
         newqty = samsies.sum(:qty)
         q.update(qty: newqty)
         mefirst = samsies.first
-        # others = samsies.where.not(mefirst)
+        others = samsies.where.not(mefirst)
         # @results = others.count
         # others.destroy_all
       end
