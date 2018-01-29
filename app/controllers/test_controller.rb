8473d50ef3394d7205d16991149aaaaca89a2a82
makerbox <mattwerth@mattwerth.com>
2018-01-29 17:59:05 +1100

    asdf

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,12 +2,13 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index 
-
+      @results = []
       thisorder = Order.find(368)
       thisorder.quantities.each do |q|
         samsies = thisorder.quantities.where(product: q.product)
         newqty = samsies.sum(:qty)
-        # q.order.quantities.where(product: q.product, id: !q).destroy
+        @results << 'SAMSIES = ' + samsies.count.to_s
+        @results << 'NEWQTY = ' + newqty.to_s
         q.update(qty: newqty)
         others = samsies.where.not(id: samsies.first.id)
         others.destroy_all
