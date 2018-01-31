0da5aeaba5f5214e2181d132e815b910354c29e2
makerbox <mattwerth@mattwerth.com>
2018-01-31 14:28:50 +1100

    asdf

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -6,11 +6,12 @@ class TestController < ApplicationController
       thisorder = Order.find(368)
       unique_products = thisorder.products.uniq
       unique_products.each do |q|
-        newqty = thisorder.quantities.where(product_id: q.id).sum(:qty)
-        original = thisorder.quantities.where(product_id: q.id).first
+        these_quantities = thisorder.quantities.where(product_id: q.id)
+        newqty = these_quantities.sum(:qty)
+        original = .first
         @results << original
         original.update(qty: newqty)
-        thisorder.quantities.all.where.not(id: original.id).destroy_all
+        these_quantities.where.not(id: original.id).destroy_all
       end
 
 
