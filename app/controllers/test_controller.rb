b075fbdecef1b84630863a15c1f3aa5466405c55
makerbox <mattwerth@mattwerth.com>
2018-01-31 13:19:21 +1100

    asdf

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -9,9 +9,7 @@ class TestController < ApplicationController
         newqty = thisorder.quantities.where(product: q).sum(:qty)
         original = thisorder.quantities.where(product: q).first
         original.update(qty: newqty)
-        thisorder.quantities.where.not(original).each do |d|
-          @results << d.qty
-        end
+        @results << thisorder.quantities.where.not(original)
       end
 
 
