6dbcf5eb36230c4130965f85119769469a12279c
makerbox <mattwerth@mattwerth.com>
2018-02-07 22:06:37 +1100

    upcase account sort

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,23 +3,14 @@ class TestController < ApplicationController
 	
 	def index 
       @results = []
-      thisorder = Order.find(368)
-      unique_products = thisorder.products.uniq
-      unique_products.each do |q|
-        these_quantities = thisorder.quantities.where(product_id: q.id)
-        newqty = these_quantities.sum(:qty)
-        original = these_quantities.first
-        original.update(qty: newqty)
-        these_quantities.all.where.not(id: original.id).destroy_all
-      end
+      
 
-
-      # dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+      @results << Account.last
+      dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
       # # @results = dbh.execute("INSERT INTO customer_master (Code, Name, Contact) VALUES ('test', 'test', 'test')")
       # # @results = dbh.execute("DELETE FROM customer_master WHERE Code = 'test'")
 
-
-      # dbh.disconnect 
+      dbh.disconnect 
       # # redirect_to 'http://roccloudy.com'
   	end
 end
