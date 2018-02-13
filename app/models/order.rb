1f7883eadd8a33db57da5362820312bbbde99838
makerbox <mattwerth@mattwerth.com>
2018-02-13 16:17:45 +1100

    asdf

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,9 +12,9 @@ class Order < ActiveRecord::Base
   		qty = q.qty.to_s
   		items << '"'+product+'","'+qty+'","","","",""'
   	end
-  	notes1 = self.notes[0,60]
+  	notes1 = self.notes[0,60].to_s
     len = self.notes.length
-    notes2 = self.notes[60,len]
+    notes2 = self.notes[60,len].to_s
   	firstline = '"'+self.user.account.company+'","","","","","","","'+filename+'","","'+self.order_number.to_s+'","","","",""
   	'
     lastline = '<F9><F4><DOWN><DOWN><DOWN><DOWN><ENTER>,"","","'+notes1+'","","'+notes2+'","","","","","","","","","","",""'
