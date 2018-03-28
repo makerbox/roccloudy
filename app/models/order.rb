074feaf465f377df4441ee5635d70db41273db60
makerbox <mattwerth@mattwerth.com>
2018-03-28 12:24:32 +1100

    kfi order

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,7 +7,7 @@ class Order < ActiveRecord::Base
   	filename = 'W' + self.id.to_s
   	path = "E:\\Attache\\Attache\\Roc\\KFIDATA\\Orders\\" + filename + ".kfi"
   	items = []
-    myquantities = self.quantities.order(brand: :asc, product_id: :asc)
+    myquantities = self.quantities.order(brand: :asc).order(product_id: :asc)
     myquantities.sort
   	myquantities.each do |q|
   		product = q.product.code.to_s
