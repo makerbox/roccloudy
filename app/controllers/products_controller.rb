0f0f44b1423cf348c041a01324b98ce85c20d7c4
makerbox <mattwerth@mattwerth.com>
2018-03-30 15:29:39 +1100

    asdf

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -230,34 +230,36 @@ end
 
   #add product to cart (used for popup ajax)
   def add_product_to_cart
-    @newquantity = Quantity.new(qty: params[:qty], product_id: Product.find(code: params[:code]), order_id: params[:order_id])
-    @newquantity.brand = @newquantity.product.group
-    if @newquantity.order == nil
-    #if there is not active order to add this to, we will just make one
-    if ((current_user.has_role? :admin) || (current_user.has_role? :rep)) && (current_user.mimic)
-      @order = Order.create(user: current_user.mimic.account.user, active: true, approved: false, complete: false)
-    else
-      @order = Order.create(user: current_user, active: true, approved: false, complete: false)
-    end
-      #update the order to have an order number based on it's ID
-      order_num = 'W' + @order.id.to_s
-      @order.update(order_number: order_num)
-      #and then add it to the new order
-      @newquantity.order = @order
-    end
+    # @newquantity = Quantity.new(qty: params[:qty], product_id: Product.find(code: params[:code]), order_id: params[:order_id])
+    # @newquantity.brand = @newquantity.product.group
+    # if @newquantity.order == nil
+    # #if there is not active order to add this to, we will just make one
+    # if ((current_user.has_role? :admin) || (current_user.has_role? :rep)) && (current_user.mimic)
+    #   @order = Order.create(user: current_user.mimic.account.user, active: true, approved: false, complete: false)
+    # else
+    #   @order = Order.create(user: current_user, active: true, approved: false, complete: false)
+    # end
+    #   #update the order to have an order number based on it's ID
+    #   order_num = 'W' + @order.id.to_s
+    #   @order.update(order_number: order_num)
+    #   #and then add it to the new order
+    #   @newquantity.order = @order
+    # end
 
-    case @newquantity.product.group
-    when 'C' , 'J'
-      group = 'roc'
-    when 'L'
-      group = 'polasports'
-    when 'LC'
-      group = 'locello'
-    when 'E' , 'R' , 'D' , 'A'
-      group = 'unity'
-    end
+    # case @newquantity.product.group
+    # when 'C' , 'J'
+    #   group = 'roc'
+    # when 'L'
+    #   group = 'polasports'
+    # when 'LC'
+    #   group = 'locello'
+    # when 'E' , 'R' , 'D' , 'A'
+    #   group = 'unity'
+    # end
+    qty = params[:qty]
+    product = params[:code]
     respond_to do |format|
-      format.json { render json: {result: 'hi'} }
+      format.json { render json: {result: qty+' '+product} }
     end
   end
 
