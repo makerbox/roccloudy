f81253c19afb1ab4f044421fa043d34140665afc
makerbox <mattwerth@mattwerth.com>
2018-03-30 19:29:31 +1100

    asdf

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -305,7 +305,7 @@ end
     prodprice = thisproduct.rrp
   end
 
-  # htmlstring << '$'+number_with_precision(prodprice, precision: 2)
+  htmlstring << '$'+number_with_precision(prodprice, precision: 2).to_s
   # subtotal = (qty * prodprice).round(2)
     
   htmlstring << '<div class="qty"> x '
