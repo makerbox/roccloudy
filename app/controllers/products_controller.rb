e301dbb40b0a2f153723d775e183ccd4925e6f27
makerbox <mattwerth@mattwerth.com>
2018-03-30 20:35:29 +1100

    asdf

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -307,7 +307,7 @@ end
     
   htmlstring += '<div class="qty"> x '
   htmlstring += qty
-  htmlstring += view_context.link_to '', edit_quantity_path(@newquantity), class: 'fa fa-pencil-alt'
+  htmlstring += '<a href="'+edit_quantity_path(@newquantity)+'" class="fa fa-pencil-alt"></a>'
   htmlstring += '</div> ------- $'+subtotal.to_s
   # htmlstring += link_to 'remove', remove_product_path(id: @newquantity.id), remote:true, data: {"qty": qty, "price": prodprice, "disable-with": "removing..."}, class: 'btn btn-warning remove-btn'
   htmlstring += '</div>'
