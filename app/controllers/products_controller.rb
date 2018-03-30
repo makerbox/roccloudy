c356e22b3e893ae54010a313b41e19386b870b51
makerbox <mattwerth@mattwerth.com>
2018-03-30 19:17:09 +1100

    asdf

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -310,7 +310,7 @@ end
     
   htmlstring << '<div class="qty"> x '+qty.to_s
   # htmlstring << link_to '', edit_quantity_path(@newquantity), class: 'fa fa-pencil-alt'
-  htmlstring << '</div> ------- $'+number_with_precision(subtotal, precision: 2)
+  # htmlstring << '</div> ------- $'+number_with_precision(subtotal, precision: 2)
   @order.total = @order.total + subtotal
   # htmlstring << link_to 'remove', remove_product_path(id: po.id), remote:true, data: {"qty": qty, "price": prodprice, "disable-with": "removing..."}, class: 'btn btn-warning remove-btn'
   htmlstring << '</div>'
