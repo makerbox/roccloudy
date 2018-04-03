ca0443c4e649aedca52e1514c4a72aff50fb774d
makerbox <mattwerth@mattwerth.com>
2018-04-03 13:59:52 +1000

    speed improvement

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,8 +31,8 @@ function removeMe(me){
 	$(me).parent().css('display','none');
 	qty = $(me).data('qty');
 	price = $(me).data('price') * qty;
-	totalqty = parseInt($('#totalqty').html(), 10);
-	totalprice = parseFloat($('#totalprice').html()).toFixed(2);
+	totalqty = parseInt($('#totalqty').text(), 10);
+	totalprice = parseFloat($('#totalprice').text()).toFixed(2);
 	console.log(totalqty + ' x ' + totalprice);
 	newqty = totalqty - qty;
 	newprice = (totalprice - price).toFixed(2);
