cba5a839597d4614aec13fab4f225b7d7fe12c8a
makerbox <mattwerth@mattwerth.com>
2018-04-10 11:48:37 +1000

    order totals

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -4,8 +4,11 @@ class TestController < ApplicationController
 	def index 
       @results = []
       
-
-      @results = Discount.all
+      product = Product.find_by(code: '7462E')
+      prod_code = product.code
+      prod_group = product.group
+      prod_cat = product.cat
+      @results = Discount.all.where('(product = ? AND (producttype = ? OR producttype = ?)) OR (product = ? AND (producttype = ? OR producttype = ?)) OR (product = ? AND (producttype = ? OR producttype = ?))', price_cat, 'cat_fixed', 'cat_percent' , prod_code , 'code_fixed', 'code_percent', prod_group, 'group_fixed', 'group_percent')
       # dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
       # # @results = dbh.execute("INSERT INTO customer_master (Code, Name, Contact) VALUES ('test', 'test', 'test')")
       # # @results = dbh.execute("DELETE FROM customer_master WHERE Code = 'test'")
