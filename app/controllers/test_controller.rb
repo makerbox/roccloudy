523005e7ee8746550ba65d1a928c94e0a45d16ba
makerbox <mattwerth@mattwerth.com>
2018-04-12 16:58:23 +1000

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,22 +2,18 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index 
-      @results = []
-      
-      dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+@results = []
+    dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
       @customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
       @customers.each do |c|
         code = c.Code.strip
-        if Account.all.find_by(code: code)
-          account = Account.all.find_by(code: code)
-          @results << c.InDispute
+        if account = Account.all.find_by(code: code)
+            # account = Account.all.find_by(code: code)
+            if c.InDispute == 1
+                @results << account.code
+                account.update(dispute: true)
+            end
         end
       end
-
-      # # @results = dbh.execute("INSERT INTO customer_master (Code, Name, Contact) VALUES ('test', 'test', 'test')")
-      # # @results = dbh.execute("DELETE FROM customer_master WHERE Code = 'test'")
-
-      dbh.disconnect 
-      # # redirect_to 'http://roccloudy.com'
   	end
 end
