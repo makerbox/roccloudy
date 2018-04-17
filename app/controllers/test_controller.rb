a41061e0a5f641dd886c724b4a31661fbf08cca5
makerbox <mattwerth@mattwerth.com>
2018-04-17 11:07:06 +1000

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -6,22 +6,23 @@ class TestController < ApplicationController
     dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
     @customers_ext = dbh.execute("SELECT * FROM customer_mastext").fetch(:all, :Struct)
     @customers_ext.each do |ce|
-      # case payterms
-      # when '1'
-      #   payterms = 'COD'
-      # when '2'
-      #   payterms = 'Set Day of Month'
-      # when '3'
-      #   payterms = 'Set Day of Next Month'
-      # when '4'
-      #   payterms = 'Day of Month after Next'
-      # when '5'
-      #   payterms = 'Number of Days'
-      # when '6'
-      #   payterms = 'Days after Month end'
-      # end
+      payterms = ce.PaymentTerms.to_s
+      case payterms
+      when '1'
+        payterms = 'COD'
+      when '2'
+        payterms = 'Set Day of Month'
+      when '3'
+        payterms = 'Set Day of Next Month'
+      when '4'
+        payterms = 'Day of Month after Next'
+      when '5'
+        payterms = 'Number of Days'
+      when '6'
+        payterms = 'Days after Month end'
+      end
       @results << ce.Code.strip
-      @results << ce.PaymentTerms.to_s
+      @results << payterms
       @results << ce.TermsDays.to_s
       @results << '------------'
     end
