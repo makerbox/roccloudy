5433402f23bd8115b2c01b40e5dc152e44ad0381
makerbox <mattwerth@mattwerth.com>
2018-04-17 10:00:53 +1000

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -6,27 +6,24 @@ class TestController < ApplicationController
     dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
     @customers_ext = dbh.execute("SELECT * FROM customer_mastext").fetch(:all, :Struct)
     @customers_ext.each do |ce|
-      code = ce.Code.strip
-      payterms = ce.PaymentTerms.to_s
-      @results << code
-      case payterms
-      when '1'
-        payterms = 'COD'
-      when '2'
-        payterms = 'Set Day of Month'
-      when '3'
-        payterms = 'Set Day of Next Month'
-      when '4'
-        payterms = 'Day of Month after Next'
-      when '5'
-        payterms = 'Number of Days'
-      when '6'
-        payterms = 'Days after Month end'
-      end
-      @results << payterms
-      if account = Account.all.find_by(code: code)
-        account.update(payterms: payterms)
-      end
+      # case payterms
+      # when '1'
+      #   payterms = 'COD'
+      # when '2'
+      #   payterms = 'Set Day of Month'
+      # when '3'
+      #   payterms = 'Set Day of Next Month'
+      # when '4'
+      #   payterms = 'Day of Month after Next'
+      # when '5'
+      #   payterms = 'Number of Days'
+      # when '6'
+      #   payterms = 'Days after Month end'
+      # end
+      @results << ce.Code.strip
+      @results << ce.PaymentTerms.to_s
+      @results << ce.TermsDays.to_s
+      @results << '------------'
     end
     dbh.disconnect 
 
