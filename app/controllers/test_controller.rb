9b210336b94107970c9433cb10303dfa7fc3ac90
makerbox <mattwerth@mattwerth.com>
2018-04-20 17:49:53 +1000

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,13 +2,25 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index 
-    @results = []
+      @results = []
       dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
       dbh.execute("UPDATE contact_details_file SET EmailAddress = 'none-00' WHERE Code = 'WANDA EY'")
+      dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
       contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
-@results << 'updated WANDA EY in attache to have email none-00'
-@results << 'now we need to run the seed, see if it worked, then copy to NZ'
-      dbh.disconnect 
+      contacts.each do |contact|
+        if contact.Active == 1
+          if account = Account.all.find_by(code: contact.Code.strip)
+            if !User.all.find_by(email: contact.EmailAddress)
+              email = contact.EmailAddress
+              thisuser = account.user
+              thisuser.email = email
+              thisuser.save(validate: false)
+              @results << email
+            end
+          end
+        end
+      end
+      dbh.disconnect
 
   end
 end
