248b93ad7ace2bc9b1b105b99a5ea178aa2bc1cd
makerbox <mattwerth@mattwerth.com>
2018-04-20 12:01:18 +1000

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -4,22 +4,23 @@ class TestController < ApplicationController
 	def index 
     @results = []
       dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-      dbh.execute("UPDATE contact_details_file SET EmailAddress = 'test' WHERE Code = 'WANDA EY'")
+      # dbh.execute("UPDATE contact_details_file SET EmailAddress = 'test' WHERE Code = 'WANDA EY'")
       contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
       contacts.each do |contact|
-      #   if contact.Active == 1
-      #     if account = Account.all.find_by(code: contact.Code.strip)
-      #       if !User.all.find_by(email: contact.EmailAddress)
-      #         email = contact.EmailAddress
-      #         account.user.update_attributes(email: email)
-      #         @results << email
-      #       end
-      #     else
-      #       @results << 'no account'
-      #     end
-      #   end
-        @results << contact.Code
-        @results << contact.EmailAddress
+        if contact.Active == 1
+          if account = Account.all.find_by(code: contact.Code.strip)
+            if !User.all.find_by(email: contact.EmailAddress)
+              email = contact.EmailAddress
+              account.user.update_attributes(email: email)
+              @results << 'account'
+              @results << email
+            end
+          else
+            @results << 'no account'
+          end
+        end
+        # @results << contact.Code
+        # @results << contact.EmailAddress
       end
       dbh.disconnect 
 
