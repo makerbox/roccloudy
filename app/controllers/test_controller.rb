8026c78e1d52de59ae8b386c9a68d12d54045082
makerbox <mattwerth@mattwerth.com>
2018-04-23 09:35:09 +1000

    email from attache

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -4,8 +4,8 @@ class TestController < ApplicationController
 	def index 
     @results = []
           dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-      transactions = dbh.execute("SELECT * FROM contact_transactions WHERE Code = 'WANDA EY'").fetch(:all, :Struct)
-      transactions.each do |t|
+      transactions = dbh.execute("SELECT * FROM contact_transactions").fetch(:all, :Struct)
+      transactions.where(Code: 'WANDA EY').each do |t|
         @results << t.TranType
       end
       dbh.disconnect 
