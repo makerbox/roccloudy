8a31b8e34eb57fda25b57bf7bb6d3bd21c3a1bad
makerbox <mattwerth@mattwerth.com>
2018-04-23 09:29:05 +1000

    email from attache

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,9 +2,13 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index 
-      @results = []
-      dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-      dbh.execute("UPDATE contact_details_file SET EmailAddress = 'none' WHERE Code = 'WANDA EY'")
-      dbh.disconnect
+    @results = []
+          dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+      transactions = dbh.execute("SELECT * FROM contact_transactions WHERE Code = 'WANDA EY'").fetch(:all, :Struct)
+      transactions.each do |t|
+        @results << t.TranType
+      end
+      dbh.disconnect 
+
   end
 end
