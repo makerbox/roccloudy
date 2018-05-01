56c68ab06a54f0db13ab75a5a3446b4467531065
makerbox <mattwerth@mattwerth.com>
2018-05-01 15:59:42 +1000

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,14 +3,38 @@ class TestController < ApplicationController
 	
 	def index 
     @results = []
-          dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-      transactions = dbh.execute("SELECT * FROM customer_transactions WHERE Code LIKE '%A LA MOD%'").fetch(:all, :Struct)
-      transactions.last(35).each do |t|
-        @results << "CODE= " + t.Code.to_s
-        @results << "DATE= " + t.TranDate.to_s
-        @results << "INVNUM= " + t.InvNum.to_s
-        @results << "DOCNUM= " + t.DocNum.to_s
-        @results << "TYPE= " + t.TranType.to_s
+      dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+      transactions = dbh.execute("SELECT * FROM product_transactions").fetch(:all, :Struct)
+      transactions.last(15).each do |t|
+        @results << "REFERENCE= " + t.Refer.to_s
+        @results << "-----------------------------------------------"
+      end
+      transactions = dbh.execute("SELECT * FROM prodxfr_head").fetch(:all, :Struct)
+      transactions.last(15).each do |t|
+        @results << "REFERENCE= " + t.Refer.to_s
+        @results << "-----------------------------------------------"
+      end
+      dbh.disconnect 
+            transactions = dbh.execute("SELECT * FROM invdtlext").fetch(:all, :Struct)
+      transactions.last(15).each do |t|
+        @results << "ORIGORDERREF= " + t.OrigOrderRef.to_s
+        @results << "-----------------------------------------------"
+      end
+      dbh.disconnect 
+            transactions = dbh.execute("SELECT * FROM invoice_header").fetch(:all, :Struct)
+      transactions.last(15).each do |t|
+        @results << "REFERENCE= " + t.Refer.to_s
+        @results << "-----------------------------------------------"
+      end
+      dbh.disconnect 
+            transactions = dbh.execute("SELECT * FROM customer_transactions").fetch(:all, :Struct)
+      transactions.last(15).each do |t|
+        @results << "REFERENCE= " + t.Refer.to_s
+        @results << "-----------------------------------------------"
+      end
+      dbh.disconnect 
+            transactions = dbh.execute("SELECT * FROM customer_std_charges").fetch(:all, :Struct)
+      transactions.last(15).each do |t|
         @results << "REFERENCE= " + t.Refer.to_s
         @results << "-----------------------------------------------"
       end
