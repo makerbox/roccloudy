7d67b1f476d1902d7968fd5b655250cc181680f9
makerbox <mattwerth@mattwerth.com>
2018-05-01 15:15:49 +1000

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -13,8 +13,23 @@ class TestController < ApplicationController
         @results << "TYPE= " + t.TranType.to_s
         @results << "REFERENCE= " + t.Refer.to_s
         @results << "AMOUNT= " + t.InvAmt.to_s
+        @results << "DISC AMNT= " + t.DiscAmt.to_s
+        @results << "COST= " + t.Cost.to_s
+        @results << "REP= " + t.SalesRep.to_s
+        @results << "TOTAL PAYMENT= " + t.TotalPayAmt.to_s
+        @results << "ACCT NAME= " + t.AccName.to_s
+        @results << "-----------------------------------------------"
       end
       dbh.disconnect 
 
+      dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+      transactions = dbh.execute("SELECT * FROM custbankdeps").fetch(:all, :Struct)
+      transactions.last(15).each do |t|
+        @results << t.CCNumber
+        @results << t.CCName
+        @results << t.CCId
+        @results << t.CCExpire
+      end
+      dbh.disconnect
   end
 end
