20366ad399fd2520c9f47084efe1735a77515e41
makerbox <mattwerth@mattwerth.com>
2018-05-01 13:59:07 +1000

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -5,10 +5,14 @@ class TestController < ApplicationController
     @results = []
           dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
       transactions = dbh.execute("SELECT * FROM customer_transactions").fetch(:all, :Struct)
-      transactions.last(5).each do |t|
-        @results << t.Code
-        @results << t.TranDate
-        @results << t.InvNum
+      transactions.last(15).each do |t|
+        @results << "CODE= " + t.Code
+        @results << "DATE= " + t.TranDate
+        @results << "INVNUM= " + t.InvNum
+        @results << "DOCNUM= " + t.DocNum
+        @results << "TYPE= " + t.TranType
+        @results << "REFERENCE= " + t.Refer
+        @results << "AMOUNT= " + t.InvAmt
       end
       dbh.disconnect 
 
