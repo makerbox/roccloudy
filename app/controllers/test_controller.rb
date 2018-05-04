61a801327c669cc647cfb7cef7adb90084aee5b2
makerbox <mattwerth@mattwerth.com>
2018-05-04 17:43:42 +1000

    initial seed credit

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -8,17 +8,21 @@ class TestController < ApplicationController
       # transactions = dbh.execute("SELECT * FROM customer_transactions WHERE TranDate >= '#{(Date.today - 30.days).strftime('%Y-%m-%d')}'").fetch(:all, :Struct)
       # transactions = dbh.execute("SELECT * FROM customer_transactions WHERE Code LIKE '%VALLEYGI%'").fetch(:all, :Struct)
 
-      customer = dbh.execute("SELECT * FROM customer_master WHERE Code LIKE '%HAVANA%'").fetch(:all, :Struct)
+      customer = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
 
       customer.each do |t|
-        @results << "CURRENT = " + t.CurrentBal.to_s
-        @results << "30 = " + t.Period1Bal.to_s
-        @results << "60 = " + t.Period2Bal.to_s
-        @results << "90 = " + t.Period3Bal.to_s
-        @results << "UNALLOCATED = " + t.UnallocBal.to_s
-        @results << "POST DATED = " + t.PostDateBal.to_s
-        @results << "TOTAL OUTSTANDING = " + (t.CurrentBal + t.Period1Bal + t.Period2Bal + t.Period3Bal).to_s
-        @results << "-----------------------------------------------"
+        if account = Account.find_by(code: t.Code)
+          account.update(current: t.CurrentBal, days30: t.Period1Bal, days60: t.Period2Bal, days90: t.Period3Bal)
+          @results << account.code
+        end
+        # @results << "CURRENT = " + t.CurrentBal.to_s
+        # @results << "30 = " + t.Period1Bal.to_s
+        # @results << "60 = " + t.Period2Bal.to_s
+        # @results << "90 = " + t.Period3Bal.to_s
+        # @results << "UNALLOCATED = " + t.UnallocBal.to_s
+        # @results << "POST DATED = " + t.PostDateBal.to_s
+        # @results << "TOTAL OUTSTANDING = " + (t.CurrentBal + t.Period1Bal + t.Period2Bal + t.Period3Bal).to_s
+        # @results << "-----------------------------------------------"
       end
       dbh.disconnect 
 
