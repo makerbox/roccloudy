b5b1d30985ebfb0e76be1399bd3ea8e538955371
makerbox <mattwerth@mattwerth.com>
2018-05-04 16:32:38 +1000

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -6,30 +6,16 @@ class TestController < ApplicationController
       dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
 
       # transactions = dbh.execute("SELECT * FROM customer_transactions WHERE TranDate >= '#{(Date.today - 30.days).strftime('%Y-%m-%d')}'").fetch(:all, :Struct)
+      # transactions = dbh.execute("SELECT * FROM customer_transactions WHERE Code LIKE '%VALLEYGI%'").fetch(:all, :Struct)
 
-      transactions = dbh.execute("SELECT * FROM customer_transactions WHERE Code LIKE '%VALLEYGI%'").fetch(:all, :Struct)
+      customer = dbh.execute("SELECT * FROM customer_master WHERE Code LIKE '%VALLEYGI%'").fetch(:all, :Struct)
 
-      @results << transactions.count
-      transactions.sort_by(&:TranDate)
-      transactions.each do |t|
+      customer.each do |t|
         @results << "CODE = " + t.Code.to_s
-        @results << "TRANDATE = " + t.TranDate.to_s
-        @results << "INVDATE = " + t.InvDate.to_s
-        @results << "INVNUM = " + t.InvNum.to_s
-        @results << "DOCNUM = " + t.DocNum.to_s
-        @results << "REFERENCE = " + t.Refer.to_s
-        @results << "TRANTYPE = " + t.TranType.to_s
-        @results << "CAT = " + t.Cat.to_s
-        @results << "INVAMT = " + t.InvAmt.to_s
-        @results << "DISCAMT = " + t.DiscAmt.to_s
-        @results << "MISCAMT = " + t.MiscAmt.to_s
-        @results << "COST = " + t.Cost.to_s
-        @results << "INVBAL = " + t.InvBal.to_s
-        @results << "TOTAL CREDIT = " + t.TotalCredit.to_s
-        @results << "TOTAL PAYMENT = " + t.TotalPayAmt.to_s
-        @results << "TOTAL ADJUSTMENT = " + t.TotalAdjustAmt.to_s
-        @results << "COMMENT = " + t.Comment.to_s
-        @results << "ZERO BAL FLAG = " + t.ZeroBalFlag.to_s
+        @results << "PERIOD 1 BALANCE = " + t.Period1Bal.to_s
+        @results << "PERIOD 2 BALANCE = " + t.Period2Bal.to_s
+        @results << "PERIOD 3 BALANCE = " + t.Period3Bal.to_s
+        @results << "CURRENT BALANCE = " + t.CurrentBal.to_s
         @results << "-----------------------------------------------"
       end
       dbh.disconnect 
