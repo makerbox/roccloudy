10d585d097d95efd792e0c27581df8c51cdd411f
makerbox <mattwerth@mattwerth.com>
2018-05-09 15:54:52 +1000

    test'

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,20 +2,8 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index 
-    @results = []
-    # -------------------------GET CUSTOMERS AND ADD / UPDATE THE DB----------------------------------
+    @results = User.all.where('email LIKE ?', '%    %')
 
-      counter = 0
-      dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-      @customers_ext = dbh.execute("SELECT * FROM customer_mastext").fetch(:all, :Struct)
-      @customers_ext.each do |ce|
-        counter += 1
-        code = ce.Code.strip
-        if code == 'SHIMMERS'
-          @results << code
-        end
-      end
-      dbh.disconnect 
 
 
   end
