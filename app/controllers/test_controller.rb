ff0310662046f6c1a293a094324631844a306534
makerbox <mattwerth@mattwerth.com>
2018-05-09 16:00:06 +1000

    test'

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,7 +2,7 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index 
-    @results = User.all.where('email LIKE ?', '%    %')
+    @results = User.all.where(account: nil)
 
 
 
