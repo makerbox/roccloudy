0dcc122820cbee59b31585499b4c6b34df97d7b0
makerbox <mattwerth@mattwerth.com>
2018-05-09 16:15:31 +1000

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,7 +2,7 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index 
-    @results = User.all.where(account: nil)
+    @results = User.all.where(email: 'staceylfennell@hotmail.com')
 
 
 
