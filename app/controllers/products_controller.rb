80e72d652906f6f286317a47a486be24e3cf6a36
makerbox <mattwerth@mattwerth.com>
2018-05-21 17:26:14 +1000

    test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy, :add_product_to_cart]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
 # GET /products
 # GET /products.json
