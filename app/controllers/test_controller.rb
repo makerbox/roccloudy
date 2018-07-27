567b4eed5c787d1c71cd71e80b8c5aff8bcdc605
makerbox <mattwerth@mattwerth.com>
2018-07-27 18:43:42 +1000

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -6,7 +6,7 @@ class TestController < ApplicationController
     dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
     @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
     @products.each do |p|
-      code = p.Code
+      code = p.Code.strip
       if Product.all.where(code: code).exists?
         @results << 'product exists'
         Product.all.find_by(code: code).update_attributes(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
