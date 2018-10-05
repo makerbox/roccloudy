2925005c2c879d47c639724506c8f7cd0b19aa0e
Matt Werth <makerbox@users.noreply.github.com>
2018-10-05 10:09:52 +1000

    Update test_controller.rb

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -10,106 +10,106 @@ class TestController < ApplicationController
 puts 'RUNNING SEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEED'
 dbh = RDBI.connect :ODBC, :db => "WHOLESALEPORTAL"
 
-      # -------------------------GET PRODUCTS AND CREATE / UPDATE PRODUCT RECORDS------------------------
-      @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-      @products.each do |p|
-        if p.Inactive == 0
-          code = p.Code.strip
-          description = p.Description.to_s.strip
-          price1 = p.SalesPrice1
-          price2 = p.SalesPrice2
-          price3 = p.SalesPrice3
-          price4 = p.SalesPrice4
-          price5 = p.SalesPrice5
-          rrp = p.SalesPrice6
-          qty = p.QtyInStock
-          qty = qty - p.QtyReserve
-          if p.AllowDisc == 1
-            allow_disc = true
-          else
-            allow_disc = false
-          end
-          group = p.ProductGroup.to_s.strip
-          pricecat = p.PriceCat.to_s.strip
-          puts pricecat
-              # # needs category
-              if Product.all.where(code: code).exists?
-                Product.all.find_by(code: code).update_attributes(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
-                # filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
-                # filename = "Z:\\AttacheBI\\Resources\\ROC\\images\\Product\\1\\" + code + ".jpg"
-                filename = "E:\\Attache\\AttacheBI\\Resources\\ROC\\Images\\product\\1\\" + code + ".jpg"
-                if File.exist?(filename)
-                  Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
-                  # stop from overloading transformations
-                else
-                  Product.all.find_by(code: code).destroy
-                end
-              else
-                newproduct = Product.new(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty, hidden: false)
-                # filename = "Z:\\AttacheBI\\Resources\\ROC\\images\\Product\\1\\" + code + ".jpg"
-                filename = "E:\\Attache\\AttacheBI\\Resources\\ROC\\Images\\product\\1\\" + code + ".jpg"
-                if File.exist?(filename)
-                  Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
-                  # stop from overloading transformations
-                  newproduct.save
-                end
+#       # -------------------------GET PRODUCTS AND CREATE / UPDATE PRODUCT RECORDS------------------------
+#       @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+#       @products.each do |p|
+#         if p.Inactive == 0
+#           code = p.Code.strip
+#           description = p.Description.to_s.strip
+#           price1 = p.SalesPrice1
+#           price2 = p.SalesPrice2
+#           price3 = p.SalesPrice3
+#           price4 = p.SalesPrice4
+#           price5 = p.SalesPrice5
+#           rrp = p.SalesPrice6
+#           qty = p.QtyInStock
+#           qty = qty - p.QtyReserve
+#           if p.AllowDisc == 1
+#             allow_disc = true
+#           else
+#             allow_disc = false
+#           end
+#           group = p.ProductGroup.to_s.strip
+#           pricecat = p.PriceCat.to_s.strip
+#           puts pricecat
+#               # # needs category
+#               if Product.all.where(code: code).exists?
+#                 Product.all.find_by(code: code).update_attributes(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
+#                 # filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
+#                 # filename = "Z:\\AttacheBI\\Resources\\ROC\\images\\Product\\1\\" + code + ".jpg"
+#                 filename = "E:\\Attache\\AttacheBI\\Resources\\ROC\\Images\\product\\1\\" + code + ".jpg"
+#                 if File.exist?(filename)
+#                   Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
+#                   # stop from overloading transformations
+#                 else
+#                   Product.all.find_by(code: code).destroy
+#                 end
+#               else
+#                 newproduct = Product.new(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty, hidden: false)
+#                 # filename = "Z:\\AttacheBI\\Resources\\ROC\\images\\Product\\1\\" + code + ".jpg"
+#                 filename = "E:\\Attache\\AttacheBI\\Resources\\ROC\\Images\\product\\1\\" + code + ".jpg"
+#                 if File.exist?(filename)
+#                   Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
+#                   # stop from overloading transformations
+#                   newproduct.save
+#                 end
                 
-              end
-            else
-              #destroy inactive
-              code = p.Code.strip
-              if thisprod = Product.all.find_by(code: code)
-                thisprod.destroy
-              end
-            end
-          end
+#               end
+#             else
+#               #destroy inactive
+#               code = p.Code.strip
+#               if thisprod = Product.all.find_by(code: code)
+#                 thisprod.destroy
+#               end
+#             end
+#           end
 
-          dbh.disconnect
+#           dbh.disconnect
 
-      #-------------------------UPDATE PRODUCTS WITH CATEGORIES -------------------------------------
-      dbh = RDBI.connect :ODBC, :db => "WHOLESALEPORTAL"
-      @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-      @categories = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
-      @categories.each do |cat|
-            if cat.CostCentre #if the prodmastext record has a category, then let's do it
-              categorycode = cat.Code.strip
-              if Product.find_by(code: categorycode) #if the product exists, let's give it the category (some products without images have no dice)
-                Product.find_by(code: categorycode).update_attributes(category: cat.CostCentre.strip)
-              end
-            end
-          end
-          dbh.disconnect
+#       #-------------------------UPDATE PRODUCTS WITH CATEGORIES -------------------------------------
+#       dbh = RDBI.connect :ODBC, :db => "WHOLESALEPORTAL"
+#       @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+#       @categories = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
+#       @categories.each do |cat|
+#             if cat.CostCentre #if the prodmastext record has a category, then let's do it
+#               categorycode = cat.Code.strip
+#               if Product.find_by(code: categorycode) #if the product exists, let's give it the category (some products without images have no dice)
+#                 Product.find_by(code: categorycode).update_attributes(category: cat.CostCentre.strip)
+#               end
+#             end
+#           end
+#           dbh.disconnect
 
-      #------------------------GET DATES AND UPDATE THE PRODUCTS WITH new_date FIELD-----------------------
-      dbh = RDBI.connect :ODBC, :db => "WHOLESALEPORTAL"
+#       #------------------------GET DATES AND UPDATE THE PRODUCTS WITH new_date FIELD-----------------------
+#       dbh = RDBI.connect :ODBC, :db => "WHOLESALEPORTAL"
 
-      @datedata = dbh.execute("SELECT * FROM produdefdata").fetch(:all, :Struct)
+#       @datedata = dbh.execute("SELECT * FROM produdefdata").fetch(:all, :Struct)
 
-      @datedata.each do |d|
-        code = d.Code.strip
-        if Product.find_by(code: code)
-          Product.find_by(code: code).update_attributes(new_date: d.DateFld)
-        end
-      end
+#       @datedata.each do |d|
+#         code = d.Code.strip
+#         if Product.find_by(code: code)
+#           Product.find_by(code: code).update_attributes(new_date: d.DateFld)
+#         end
+#       end
 
-      dbh.disconnect
+#       dbh.disconnect
 
-      # ------------------------GET FABS, CONNECT THEM, AND UPDATE THE PRODUCTS-----------------------
-      dbh = RDBI.connect :ODBC, :db => "WHOLESALEPORTAL"
+#       # ------------------------GET FABS, CONNECT THEM, AND UPDATE THE PRODUCTS-----------------------
+#       dbh = RDBI.connect :ODBC, :db => "WHOLESALEPORTAL"
 
-      @fabdata = dbh.execute("SELECT * FROM produdefdata").fetch(:all, :Struct)
-      fab = ''
-      @fabdata.each do |d|
-        code = d.Code.strip
-        if Product.find_by(code: code)
-          if !d.TextFld.blank?
-            fab = d.TextFld.strip
-            Product.find_by(code: code).update_attributes(fab: fab)
-          end
-        end
-      end
+#       @fabdata = dbh.execute("SELECT * FROM produdefdata").fetch(:all, :Struct)
+#       fab = ''
+#       @fabdata.each do |d|
+#         code = d.Code.strip
+#         if Product.find_by(code: code)
+#           if !d.TextFld.blank?
+#             fab = d.TextFld.strip
+#             Product.find_by(code: code).update_attributes(fab: fab)
+#           end
+#         end
+#       end
 
-      dbh.disconnect
+#       dbh.disconnect
 
       # ------------------------DISCOUNTS---------------------------------------------------------
        Discount.destroy_all #wipe existing discounts in case of some deletions in Attache
@@ -164,54 +164,58 @@ dbh = RDBI.connect :ODBC, :db => "WHOLESALEPORTAL"
           end
 
           discounts.each do |d|
-            if (d.LevelNum >= 1) 
-              percentage = d.DiscPerc1
-              fixed = d.Price1
-              fixedprice = d.PriceCode1
-              level = 1
-              maxqty = d.MaxQty1
-              disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-            end
-            if (d.LevelNum >= 2) 
-              percentage = d.DiscPerc2
-              fixed = d.Price2
-              fixedprice = d.PriceCode2
-              level = 2
-              maxqty = d.MaxQty2
-              disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-            end
-            if (d.LevelNum >= 3) 
-              percentage = d.DiscPerc3
-              fixed = d.Price3
-              fixedprice = d.PriceCode3
-              level = 3
-              maxqty = d.MaxQty3
-              disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-            end
-            if (d.LevelNum >= 4) 
-              percentage = d.DiscPerc4
-              fixed = d.Price4
-              fixedprice = d.PriceCode4
-              level = 4
-              maxqty = d.MaxQty4
-              disco(percentage, fixed, fixedprice, level, maxqty, d.customertype, d.producttype, d.customer, d.product)
-            end
-            if (d.LevelNum >= 5) 
-              percentage = d.DiscPerc5
-              fixed = d.Price5
-              fixedprice = d.PriceCode5
-              level = 5
-              maxqty = d.MaxQty5
-              disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-            end
-            if (d.LevelNum >= 6) 
-              percentage = d.DiscPerc6
-              fixed = d.Price6
-              fixedprice = d.PriceCode6
-              level = 6
-              maxqty = d.MaxQty6
-              disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-            end
+		  @results << d.LevelNum
+		  @results << d.DiscPerc1
+		  @results << d.PriceCode1
+		  
+#             if (d.LevelNum >= 1) 
+#               percentage = d.DiscPerc1
+#               fixed = d.Price1
+#               fixedprice = d.PriceCode1
+#               level = 1
+#               maxqty = d.MaxQty1
+#               disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
+#             end
+#             if (d.LevelNum >= 2) 
+#               percentage = d.DiscPerc2
+#               fixed = d.Price2
+#               fixedprice = d.PriceCode2
+#               level = 2
+#               maxqty = d.MaxQty2
+#               disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
+#             end
+#             if (d.LevelNum >= 3) 
+#               percentage = d.DiscPerc3
+#               fixed = d.Price3
+#               fixedprice = d.PriceCode3
+#               level = 3
+#               maxqty = d.MaxQty3
+#               disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
+#             end
+#             if (d.LevelNum >= 4) 
+#               percentage = d.DiscPerc4
+#               fixed = d.Price4
+#               fixedprice = d.PriceCode4
+#               level = 4
+#               maxqty = d.MaxQty4
+#               disco(percentage, fixed, fixedprice, level, maxqty, d.customertype, d.producttype, d.customer, d.product)
+#             end
+#             if (d.LevelNum >= 5) 
+#               percentage = d.DiscPerc5
+#               fixed = d.Price5
+#               fixedprice = d.PriceCode5
+#               level = 5
+#               maxqty = d.MaxQty5
+#               disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
+#             end
+#             if (d.LevelNum >= 6) 
+#               percentage = d.DiscPerc6
+#               fixed = d.Price6
+#               fixedprice = d.PriceCode6
+#               level = 6
+#               maxqty = d.MaxQty6
+#               disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
+#             end
           end
 
           dbh.disconnect 
