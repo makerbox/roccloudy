3002e65a47ee3d4c586d8ba323d097c3f64f5e86
Matt Werth <makerbox@users.noreply.github.com>
2018-10-12 17:06:41 +1100

    Update test_controller.rb

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,8 +1,9 @@
 class TestController < ApplicationController
       skip_before_action :authenticate_user!
+      def index
+            
       @results = Product.all
-
-      
-
-   
+            
+            
+      end
 end
